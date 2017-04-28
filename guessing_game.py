0f4af948e03c843499a43e6098d0e596eed31320
Uros Jevremovic <urosh43@gmail.com>
2017-04-28 13:37:10 +0200

    added sets

--- guessing_game.py
+++ guessing_game.py
@@ -38,13 +38,13 @@ def draw(bad_guesses, good_guesses, secret_word):
     print('')
 
 
-def get_guess(bad_guesses, good_guesses):
+def get_guess(guesses):
     while True:
         guess = input("Guess a letter:  ").lower()
 
         if len(guess) != 1:
             print("you can only guess a single letter!")
-        elif guess in bad_guesses or guess in good_guesses:
+        elif guess in guesses:
             print("You've already guessed that letter!")
         elif not guess.isalpha():
             print("You can only guess letters!")
@@ -55,25 +55,22 @@ def get_guess(bad_guesses, good_guesses):
 def play(done):
     clear()
     secret_word = random.choice(fruits)
-    bad_guesses = []
-    good_guesses = []
+    bad_guesses = set()
+    good_guesses = set()
+    word_set = set(secret_word)
 
     while True:
         draw(bad_guesses, good_guesses, secret_word)
-        guess = get_guess(bad_guesses, good_guesses)
+        guess = get_guess(bad_guesses | good_guesses)
 
-        if guess in secret_word:
-            good_guesses.append(guess)
-            found = True
-            for letter in secret_word:
-                if letter not in good_guesses:
-                    found = False
-            if found:
+        if guess in word_set:
+            good_guesses.add(guess)
+            if not word_set.symmetric_difference(good_guesses):
                 print('You win')
                 print(f'The secret word was {secret_word}')
                 done = True
         else:
-            bad_guesses.append(guess)
+            bad_guesses.add(guess)
             if len(bad_guesses) == 7:
                 draw(bad_guesses, good_guesses, secret_word)
                 print('You lost!')
